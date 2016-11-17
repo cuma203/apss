9429944ae9216943fdb4a05925a53e20a641427a
Kodi <cuma203@wp.pl>
2016-11-17 23:29:41 +0100

    edycja pliku controller.php

--- application/controllers/controller.php
+++ application/controllers/controller.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo 'testy git';
+        echo 'testy git zmiana do kolejnego commitu i brancha';
         ?>
     </body>
 </html>
